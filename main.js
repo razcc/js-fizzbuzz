ce88443367af091730f80646af4fae34ffd3533c
Rafael Leonardi <rafael.leonardi98@gmail.com>
2022-10-23 19:53:47 +0200

    Primo tentativo di stampa Fizz Buzz

--- main.js
+++ main.js
@@ -1,8 +1,39 @@
-let divNumero= document.getElementById("printNumber") 
 
- for (let i =0; i<=100; i++){
-     console.log(i)
+function creaSerie() {
 
-     let stringa = `Il tuo numero è: ${i}`;
-    divNumero.innerHTML += stringa;
- }
+    let div = document.getElementById("printNumber");
+
+    for (let i = 0; i <= 100; i++) {
+        console.log(i)
+        
+        if ( i % 3 === 0) {
+            let i= "Fizz"
+            console.log(i)
+            div.innerHTML += i;
+            
+        }
+        else if (i % 5 === 0) {
+            let i= "Buzz"
+            console.log(i)
+            div.innerHTML += i;
+           
+        }
+        else if ( i % 15 === 0) {
+            let i= "FizzBuzz"
+            console.log(i)
+            div.innerHTML += i;
+            
+        }
+        else{
+          
+            div.innerHTML += `<p>${i}</p>`;
+        }
+    }
+    
+
+
+
+
+}
+
+
